44844edbcdafb0fea2b4b30e98321117686c00f4
NadimRai <rai_nadim@yahoo.com>
2018-01-13 22:11:35 +0800

    Find the longest word in a string

--- FrontEndDevelopment/BasicAlgorithmScripting/Find_The_Longest_Word_In_A_String.js
+++ FrontEndDevelopment/BasicAlgorithmScripting/Find_The_Longest_Word_In_A_String.js
@@ -10,3 +10,18 @@ function findLongestWord(str) {
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
+
+//Alternate Solution
+
+function findLongestWord(str) {
+  var strArr = str.split(' ');
+ 
+  var longestString = strArr.reduce(function(longestName, name){
+    longestName = longestName || '';
+    if(name.length > longestName.length){
+      longestName = name;
+    }
+    return longestName;
+  });
+  return longestString.length;
+}
